73f28779a9203081630c676a6ab9473f2de9faea
Roshan Kumar Sharma <roshan@conscent.ai>
2025-01-13 19:15:09 +0530

    font size small

--- app/src/main/java/com/example/movieapp/presentation/MovieCard.kt
+++ app/src/main/java/com/example/movieapp/presentation/MovieCard.kt
@@ -5,20 +5,14 @@ import androidx.compose.animation.core.animateFloat
 import androidx.compose.animation.core.infiniteRepeatable
 import androidx.compose.animation.core.rememberInfiniteTransition
 import androidx.compose.animation.core.tween
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentHeight
-import androidx.compose.material.Icon
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Star
-import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
@@ -37,6 +31,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import coil.compose.AsyncImage
 import com.example.movieapp.R
 import com.example.movieapp.model.Search
@@ -71,7 +66,7 @@ fun MovieCard(movies: Search, index: Int, onItemClicked: (Int) -> Unit) {
                 contentScale = ContentScale.Crop,
                 modifier = Modifier
                     .fillMaxWidth()
-                    .height(200.dp)
+                    .height(300.dp)
                     .run { if (isAnimate) rotate(rotate) else this },
                 model = movies.Poster,
                 contentDescription = movies.Title,
@@ -87,6 +82,7 @@ fun MovieCard(movies: Search, index: Int, onItemClicked: (Int) -> Unit) {
                 Text(
                     text = movies.Title,
                     style = MaterialTheme.typography.titleLarge.copy(
+                        fontSize = 18.sp,
                         fontWeight = FontWeight.Bold,
                         color = Color.White
                     ),
@@ -98,7 +94,8 @@ fun MovieCard(movies: Search, index: Int, onItemClicked: (Int) -> Unit) {
                     text = movies.Year,
                     style = MaterialTheme.typography.titleMedium.copy(color = Color.White),
                     overflow = TextOverflow.Ellipsis,
-                    maxLines = 1
+                    maxLines = 1,
+                    fontSize = 14.sp,
                 )
 
             }
