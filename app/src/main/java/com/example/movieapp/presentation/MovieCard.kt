f605a5cd77289d73d78cd32248b4b9cce092989e
Roshan Kumar Sharma <roshan@conscent.ai>
2025-01-13 18:59:18 +0530

    SearchScreen UI configuration change, MovieCard UI

--- app/src/main/java/com/example/movieapp/presentation/MovieCard.kt
+++ app/src/main/java/com/example/movieapp/presentation/MovieCard.kt
@@ -1,27 +1,109 @@
 package com.example.movieapp.presentation
 
+import androidx.compose.animation.core.LinearEasing
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.infiniteRepeatable
+import androidx.compose.animation.core.rememberInfiniteTransition
+import androidx.compose.animation.core.tween
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.Icon
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.drawBehind
+import androidx.compose.ui.draw.rotate
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
+import coil.compose.AsyncImage
+import com.example.movieapp.R
 import com.example.movieapp.model.Search
 
 
 @Composable
-fun MovieCard(movies: Search) {
+fun MovieCard(movies: Search, index: Int, onItemClicked: (Int) -> Unit) {
 
+    val isAnimate by remember { mutableStateOf(false) }
+    val transition = rememberInfiniteTransition(label = "")
+    val rotate by transition.animateFloat(
+        initialValue = 0f,
+        targetValue = 360f,
+        animationSpec = infiniteRepeatable(
+            animation = tween(1000, easing = LinearEasing)
+        ), label = ""
+    )
 
-    Card(modifier = Modifier
-        .fillMaxWidth()
-        .padding(16.dp)) {
-        Column(modifier = Modifier.padding(9.dp)) {
-            Text(text = movies.Title, color = Color.Black)
-            Text(text = movies.Title, color = Color.Black)
+    ElevatedCard(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxWidth()
+            .wrapContentHeight(),
+        onClick = {
+            onItemClicked.invoke(index)
+        },
+        elevation = CardDefaults.elevatedCardElevation(defaultElevation = 4.dp)
+    ) {
+
+        Box(modifier = Modifier.fillMaxSize(), contentAlignment = Alignment.BottomStart) {
+            AsyncImage(
+                contentScale = ContentScale.Crop,
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .height(200.dp)
+                    .run { if (isAnimate) rotate(rotate) else this },
+                model = movies.Poster,
+                contentDescription = movies.Title,
+                error = painterResource(R.drawable.ic_launcher_background),
+            )
+
+            Column(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .drawBehind { drawRect(color = Color(0x59000000)) }
+                    .padding(8.dp),
+            ) {
+                Text(
+                    text = movies.Title,
+                    style = MaterialTheme.typography.titleLarge.copy(
+                        fontWeight = FontWeight.Bold,
+                        color = Color.White
+                    ),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+                Spacer(modifier = Modifier.height(2.dp))
+                Text(
+                    text = movies.Year,
+                    style = MaterialTheme.typography.titleMedium.copy(color = Color.White),
+                    overflow = TextOverflow.Ellipsis,
+                    maxLines = 1
+                )
+
+            }
         }
+
+
     }
 }
--- app/src/main/java/com/example/movieapp/presentation/SearchMoviesScreen.kt
+++ app/src/main/java/com/example/movieapp/presentation/SearchMoviesScreen.kt
@@ -2,11 +2,17 @@ package com.example.movieapp.presentation
 
 import android.util.Log
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.lazy.staggeredgrid.LazyVerticalStaggeredGrid
+import androidx.compose.foundation.lazy.staggeredgrid.StaggeredGridCells
+import androidx.compose.foundation.lazy.staggeredgrid.itemsIndexed
 import androidx.compose.material.CircularProgressIndicator
 import androidx.compose.material.Scaffold
 import androidx.compose.runtime.Composable
@@ -14,6 +20,9 @@ import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.Color.Companion.Blue
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
 import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
 
@@ -24,11 +33,13 @@ fun SearchMoviesScreen(
    val movies = sharedViewModel.movies.collectAsState()
 
 
-//    if (movies.value.isLoading){
-//        Box(modifier = Modifier.fillMaxSize()){
-////            CircularProgressIndicator()
-//        }
-//    }
+    if (movies.value.isLoading){
+        Box(modifier = Modifier.fillMaxSize().background(color = Color.Gray)){
+            Dialog(onDismissRequest = { }) {
+                CircularProgressIndicator(color = Blue)
+            }
+        }
+    }
 
     Scaffold(
         topBar = {
@@ -42,13 +53,20 @@ fun SearchMoviesScreen(
             )
         }
     ) { contentPadding->
-        LazyColumn(
-            Modifier
+        LazyVerticalStaggeredGrid(
+            modifier = Modifier
                 .padding(contentPadding)
-                .background(color = Color.Gray)) {
+                .background(color = Color.Gray),
+            columns = StaggeredGridCells.Fixed(2),
+            contentPadding = PaddingValues(8.dp),
+            horizontalArrangement = Arrangement.spacedBy(8.dp),
+            verticalItemSpacing = 8.dp
+        ) {
             movies.value.item?.Search.let { it->
-                items(it ?: emptyList()){ movies->
-                    MovieCard(movies = movies)
+                itemsIndexed(it ?: emptyList()){ index,movies->
+                    MovieCard(movies = movies,index, onItemClicked = {
+
+                    })
                 }
 
             }
